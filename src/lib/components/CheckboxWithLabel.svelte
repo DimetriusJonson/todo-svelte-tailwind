5a244d3f32e548a66404e1aa4e7a20e5b2b5d5fc
Dmitriy V. Isaev <shtirlizlive@yandex.ru>
2026-06-04 16:57:58 +0300

    fix ui checkbox

--- src/lib/components/CheckboxWithLabel.svelte
+++ src/lib/components/CheckboxWithLabel.svelte
@@ -5,7 +5,9 @@
 <div class="flex relative items-center">
     <input class={`peer h-8 w-8 cursor-pointer appearance-none rounded border-2
       border-slate-500
-      bg-dark-bg transition-colors 
+      dark:bg-dark-bg 
+      bg-white
+      transition-colors 
       checked:border-yellow-400 
       checked:bg-yellow-400 
       checked:hover:border-yellow-500 
@@ -16,14 +18,13 @@
       checked:focus:ring-yellow-200 
       focus-visible:outline-none 
       disabled:cursor-not-allowed 
-      disabled:border-slate-100 
-      disabled:bg-slate-50 `.concat(className)} 
+      disabled:dark:bg-dark-bg `.concat(className)} 
     id={name.toLowerCase().replaceAll(" ", "")}
     type="checkbox"
     bind:checked
     {name} />
     <label class="h-full cursor-pointer pl-2 text-gray-700 dark:text-gray-50 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400" for={name.toLowerCase().replaceAll(" ", "")}>{label}</label>
-    <svg class="pointer-events-none absolute left-0 top-1 h-8 w-8 -rotate-90 fill-white stroke-gray-700 opacity-0 transition-all duration-300 peer-checked:rotate-0 peer-checked:opacity-100 peer-disabled:cursor-not-allowed" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" aria-labelledby="title-1 description-1" role="graphics-symbol">
+    <svg class="pointer-events-none absolute left-0 top-1 h-8 w-8 -rotate-90 fill-white stroke-gray-700 opacity-0 transition-all duration-300 peer-checked:rotate-0 peer-checked:opacity-100 peer-disabled:cursor-not-allowed" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="graphics-symbol">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M12.8116 5.17568C12.9322 5.2882 13 5.44079 13 5.5999C13 5.759 12.9322 5.91159 12.8116 6.02412L7.66416 10.8243C7.5435 10.9368 7.37987 11 7.20925 11C7.03864 11 6.87501 10.9368 6.75435 10.8243L4.18062 8.42422C4.06341 8.31105 3.99856 8.15948 4.00002 8.00216C4.00149 7.84483 4.06916 7.69434 4.18846 7.58309C4.30775 7.47184 4.46913 7.40874 4.63784 7.40737C4.80655 7.406 4.96908 7.46648 5.09043 7.57578L7.20925 9.55167L11.9018 5.17568C12.0225 5.06319 12.1861 5 12.3567 5C12.5273 5 12.691 5.06319 12.8116 5.17568Z" />
     </svg>
 </div>
