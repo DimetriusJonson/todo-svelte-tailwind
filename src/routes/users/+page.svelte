fc597781449fc33365be173285ddb6a2953d5346
Dmitriy V. Isaev <shtirlizlive@yandex.ru>
2026-06-04 20:31:50 +0300

    fix users

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -3,14 +3,26 @@
     import { getUsers } from "$lib/remote/user.remote";
 </script>
 
-<section class="section is-paddingless">
-    <div class="container">
+<section>
+    <div class="container mx-auto p-4">
         <MainTitle title="Пользователи" />
 
-        <div>
-            {#each await getUsers() as { id, name }}
-                <div>{id}: {name}</div>
-            {/each}
-        </div>
+        <table class="table-auto w-full text-left dark:text-gray-200 text-xs md:text-base">
+            <thead>
+                <tr class="border-b dark:border-gray-600">
+                <th class="px-4 py-4">{"Id"}</th>
+                <th class="px-4">{"Имя"}</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each await getUsers() as { id, name }}
+                    <tr class="dark:even:bg-gray-800/30 border-b dark:border-gray-600">
+                        <td class="px-4 py-2">{id}</td>
+                        <td class="px-4 py-2">{name}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+
     </div>
 </section>
